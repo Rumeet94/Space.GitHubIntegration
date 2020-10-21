146cf5bf4870c52f0beac9164f934f0c912b7513
egorbunov <egorbunov@GORBUNOV>
2020-10-21 20:00:28 +0300

    initial commit

---
+++ Space.GitHubIntegration/Space.GitHubIntegration/Controllers/GitHubApiController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Space.GitHubIntegration.Controllers {
+	[ApiController]
+	[Route("github")]
+	public class GitHubApiController {
+		[HttpPost]
+		public void Post() {
+			
+		}
+	}
+}
